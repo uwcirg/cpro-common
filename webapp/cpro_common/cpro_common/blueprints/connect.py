2a78de94371831554400e835a286f31dc3ce5f92
Piotr Mankowski <pmanko@uw.edu>
2018-03-22 00:19:27 -0500

    More work for #5

--- webapp/cpro_common/cpro_common/blueprints/connect.py
+++ webapp/cpro_common/cpro_common/blueprints/connect.py
@@ -19,10 +19,35 @@ def launch():
     oauth.epic.api_base_url = request.args.get("iss")
     oauth.epic.access_token_url = parsed_body.findall('.//*[@url="authorize"]*')[0].attrib['value']
     oauth.epic.authorize_url = parsed_body.findall('.//*[@url="token"]*')[0].attrib['value']
-
-    redirect_uri = url_for('connect.authorize', name='epic', _external=True)
+    oauth.epic.launch = request.args.get('launch')
+    oauth.epic.state = '1342' # TODO: change!
+    
+    ccc
+    redirect_uri = url_for('connect.callback', name='epic', _external=True)
 
     return oauth.epic.authorize_redirect(redirect_uri)
+
+@bp.route('/callback/epic')
+def callback():
+    state = request.args.get("state")
+    code = request.args.get("code")
+
+    client = oauth.epic
+    token = client.authorize_access_token()
+    
+    ## Get patient id and other params here too! and save them
+
+    # jj
+    # # Get the relevant user info
+    # # user_info = service.profile()
+
+    # # token['sub'] = user_info.sub
+    
+    Connect.create_token(name, token, current_user)
+    
+
+    # # Redirect to correct cPRO App
+    # return redirect(url_for('.list_connects'))
 
 # @bp.route('')
 # @require_login
@@ -37,11 +62,11 @@ def launch():
 #     )
 
 
-@bp.route('/bind/<name>')
-@require_login
-def bind(name):
-    callback_uri = url_for('.authorize', name=name, _external=True)
-    return service.authorize_redirect(callback_uri)
+# @bp.route('/bind/<name>')
+# @require_login
+# def bind(name):
+#     callback_uri = url_for('.authorize', name=name, _external=True)
+#     return service.authorize_redirect(callback_uri)
 
 # @app.route('/login/<name>')
 # def login(name):
@@ -49,22 +74,6 @@ def bind(name):
 #     redirect_uri = url_for('authorize', name=name, _external=True)
 #     return client.authorize_redirect(redirect_uri)
 
-
-@bp.route('/authorize/<name>')
-@require_login
-def authorize(name):
-    client = oauth.create_client(name)
-    token = client.authorize_access_token()
-    
-    # Get the relevant user info
-    # user_info = service.profile()
-
-    # token['sub'] = user_info.sub
-    
-    Connect.create_token(name, token, current_user)
-    
-    # Redirect to correct cPRO App
-    return redirect(url_for('.list_connects'))
 
 
 def _get_service_or_404(name):
